7f4cb8ebe0fb51792d9b1a24fff26596b294e6b5
Mikkel Valentin Sørensen <mvs@codehouse.com>
2018-05-22 18:28:09 +0200

    Added EnsuredImmutableAndSerializable<T>

--- source/MikValSor.EnsuredImmutable/EnsuredImmutable.cs
+++ source/MikValSor.EnsuredImmutable/EnsuredImmutable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MikValSor.Runtime.Serialization;
+using System;
 
 namespace MikValSor.Immutable
 {
@@ -29,6 +30,11 @@ namespace MikValSor.Immutable
 		public static EnsuredImmutable<T> Create<T>(T immutable)
 		{
 			ImmutableValidator.Instance.EnsureImmutable(immutable);
+
+			if (SerializableValidator.Instance.IsSerializable(immutable))
+			{
+				return new EnsuredImmutableAndSerializable<T>(immutable);
+			}
 			return new EnsuredImmutable<T>(immutable);
 		}
 	}
@@ -36,16 +42,22 @@ namespace MikValSor.Immutable
 	/// <summary>
 	///		Class used to ensure that objects are immutable
 	/// </summary>
-	public sealed class EnsuredImmutable<T> : EnsuredImmutable
+	public class EnsuredImmutable<T> : EnsuredImmutable
 	{
 		/// <summary>
 		///		Value that is ensured immutable.
 		/// </summary>
 		public readonly T Value;
 
-		internal EnsuredImmutable(T value)
+		/// <summary>
+		///		Indicate if value is serializable.
+		/// </summary>
+		public readonly bool Serializable;
+
+		internal EnsuredImmutable(T value, bool serializable = false)
 		{
 			Value = value;
+			Serializable = serializable;
 		}
 
 		/// <summary>
@@ -57,6 +69,32 @@ namespace MikValSor.Immutable
 		public EnsuredImmutable<T> EnsureImmutable()
 		{
 			return this;
+		}
+
+		/// <summary>
+		///		Ensures that Value is serializable.
+		/// </summary>
+		/// <returns>
+		///		Returns EnsuredImmutableAndSerializable´1 with the Value.
+		/// </returns>
+		public EnsuredImmutableAndSerializable<T> EnsureSerializable()
+		{
+			var reply = this as EnsuredImmutableAndSerializable<T>;
+			if (reply != null) return reply;
+
+			//Not serializable.
+			SerializableValidator.Instance.EnsureSerializable(Value);
+			throw new ApplicationException("SerializableValidator.Instance.EnsureSerializable(Value) should have thrown exception.");
+		}
+	}
+
+	/// <summary>
+	///		Class used to ensure that objects are immutable
+	/// </summary>
+	public sealed class EnsuredImmutableAndSerializable<T> : EnsuredImmutable<T>
+	{
+		internal EnsuredImmutableAndSerializable(T value) : base(value, true)
+		{
 		}
 	}
 }
